a36252ae669eac8672ae6dacf0a964e08be425d6
kjwsl <kjwdev01@gmail.com>
2024-10-15 13:14:22 +0900

    Trying to integrate with Android

--- fakegps.py
+++ fakegps.py
@@ -1,12 +1,15 @@
+import pty
+import argparse
 import os
 import time
 import threading
-import pty
-import argparse
 import sys
 import random
 from datetime import datetime
 import stat
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(script_dir, "lib"))
 
 
 def calculate_checksum(nmea_sentence):
