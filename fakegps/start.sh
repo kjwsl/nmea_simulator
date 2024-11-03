9e2bb921b829839faa9aae22d9e77ef9cbc73509
kjwsl <kjwdev01@gmail.com>
2024-11-03 20:33:59 +0900

    update: wow

--- fakegps/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-socat -dd pty,raw,echo=0,link=/tmp/gps_input pty,raw,echo=0,link=/tmp/gps_output &
-sleep 1
-build/fakegps --serial /tmp/gps_input
--- fakegps/start_py.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-socat -dd pty,raw,echo=0,link=/tmp/gps_input pty,raw,echo=0,link=/tmp/gps_output &
-python3 fakegps.py --serial /tmp/gps_input
